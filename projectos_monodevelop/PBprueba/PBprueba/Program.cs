029bf61d1e0f6de3bc1256717d6ef7ce65afe14a
philippepego95 <p.gonzalezmiralles@gmail.com>
2016-09-20 00:28:29 +0200

    proves de conexio a bd en un select

--- projectos_monodevelop/PBprueba/PBprueba/Program.cs
+++ projectos_monodevelop/PBprueba/PBprueba/Program.cs
@@ -5,18 +5,19 @@ namespace PBdprueba
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
-		{
+		public static void Main (string[] args){
+			String sql =("Database=dbprueba; User Id=root; Password=sistemas");
 
 
-			/*Console.WriteLine ("Probando acceso a dbPrueba");
+		/*	Console.WriteLine ("Probando acceso a dbPrueba");
 			MySqlConnection mySqlConnection = new MySqlConnection 
 				("Database=dbprueba; User Id=root; Password=sistemas");
 			mySqlConnection.Open ();
 			//operacioins
 			Console.WriteLine("conectant...");
 			mySqlConnection.Close ();*/
-
+			Boolean bandera=true;
+			do{
 			Console.WriteLine ("Tria la opció que vols fer: " +
 				"\n0.Eixir" +
 				"\n1.Nou" +
@@ -27,7 +28,7 @@ namespace PBdprueba
 			switch (Console.Read())
 			{
 			case '0': Console.Write("\n Eixint..");
-				// Continuar lógica y extraer métodos //
+					bandera=false;
 				break;
 			case '1': Console.Write("\n Creant..");
 				// Continuar lógica y extraer métodos //
@@ -39,12 +40,46 @@ namespace PBdprueba
 				// Continuar lógica y extraer métodos //
 				break;
 			case '4': Console.Write("\n Llistant..");
-				// Continuar lógica y extraer métodos //
+
+
+					Console.WriteLine("Quin id vols mostrar");
+
+					int id = Console.Read();
+					Console.Write("El numero és "+id);
+
+					using(MySqlConnection cn = new MySqlConnection(sql)){
+						cn.Open();
+						MySqlCommand cmd =new MySqlCommand("Select * From categoria",cn);
+						MySqlDataReader dr =cmd.ExecuteReader();
+						if(dr.Read()){
+							
+							Console.WriteLine(Convert.ToString(dr[id]));
+						//	Console.WriteLine(Convert.ToString(dr["nombre"]));
+
+						}
+						cn.Close();
+					}
+					/*	
+					//Console.WriteLine (Probando acceso a dbPrueba");
+					MySqlConnection mySqlConnection = new MySqlConnection 
+						("Database=dbprueba; User Id=root; Password=sistemas");
+
+					MySqlCommand cmd= new MySqlCommand();
+					MySqlDataReader reader;
+
+					cmd.CommandText= "Select * FROM categoria";
+					cmd.CommandType= System.Data.CommandType.Text;
+					cmd.Connection = mySqlConnection;
+					reader = cmd.ExecuteReader();
+					//Console.WriteLine("conectant...");
+					mySqlConnection.Close ();*/
+
 				break;
 			} 
 			Console.ReadKey();
+			}while (bandera==true);
 
 		}
-
 	}
+		
 }
